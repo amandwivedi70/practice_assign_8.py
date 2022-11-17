e4d96fb0a2a196ad5959b8a63ef4e262dcf44ce0
Aman dwivedi <111753149+amandwivedi70@users.noreply.github.com>
2022-11-17 21:42:46 +0530

    Add files via upload

---
+++ practice_assign_8.py
@@ -0,0 +1,73 @@
+# question 1
+
+i=1
+while i<=5 :
+    print("mysirG")
+    i+=1
+
+# quesation 2
+
+i=1
+while i<=10 :
+    print(i+0)
+    i+=1
+
+# question 3
+
+i=10
+while i>=1 :
+    print(i+0)
+    i-=1
+
+# question 4
+
+i=1
+while i<=20 :
+    print(i+0)
+    i+=2
+
+
+# question 5
+i=20
+while i>=0 :
+    print(i-1)
+    i-=2
+
+
+# question 6
+
+i=2
+while i<=20 :
+    print(i+0)
+    i+=2
+
+
+# question 7
+
+i=20
+while i>=0 :
+    print(i-0)
+    i-=2
+
+
+# question 8
+
+i=1
+while i<=10 :
+    print((i+0)**2)
+    i+=1
+
+# question 9
+
+i=1
+while i<=10 :
+    print((i+0)**3)
+    i+=1
+
+
+# question 10
+
+i=5
+while i<=50 :
+    print(i+0)
+    i+=5
